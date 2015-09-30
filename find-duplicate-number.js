fab6481baeeea49a8d38a1e1a9b201a170e75ca8
spmassey <stevepmassey@gmail.com>
2015-09-29 20:16:06 -0400

    Create find-duplicate-number.js

---
+++ find-duplicate-number.js
@@ -0,0 +1,13 @@
+var nums = [6, 4, 10, 2, 3, 9, 121, 542, 1, 11, 10];
+// O(n) time complexity
+function findDuplicate(nums) {
+	var i;
+	nums.sort();
+	for (i = 0; i < nums.length; ++i) {
+		if (typeof nums[i+1] == 'number' && nums[i] == nums[i+1]) {
+			return nums[i];
+		}
+	}
+	return -1;
+}
+findDuplicate(nums);
